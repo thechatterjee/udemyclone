e7a2f05cdb95a0572f3aa951ec3e0acdf8a523f6
sumit <sumitchatterjee502@gmail.com>
2022-06-15 00:10:24 +0530

    React start class

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import HomePage from './Module/Admin/Pages/HomePage'
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="App">
+        Sumit
+      </div>
+      <div>
+        Subha
+      </div>
+      
+        <HomePage/>
+             
+    </React.Fragment>
+  );
+}
+
+export default App;
